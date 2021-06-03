98cc9fd9c11d7b1aa8398add2eb2ee419c4056d9
MattiaRipoli <mattia.ripoli001@studenti.uniparthenope.it>
2021-06-03 19:11:40 +0200

    variable number services added second html

--- static/js/main.js
+++ static/js/main.js
@@ -89,6 +89,8 @@ function getNominatimData(lat, lon) {
 
 
 function searchNominatimData(query) {
+    if (query.length == 0)
+        return
     let NominatimAPI = "https://nominatim.openstreetmap.org/search?q=" + query.toString() +"&format=jsonv2&accept-language=it&addressdetails=1"
     console.log(NominatimAPI)
     let request = new XMLHttpRequest()
